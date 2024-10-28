31227da19a895dec19ce2f2d2faca64d3447ff34
Alexsky347 <ledain.alexis@gmail.com>
2024-10-28 17:21:29 +0100

    add form and starting tab

--- src/app/features/rent/rent.component.ts
+++ src/app/features/rent/rent.component.ts
@@ -1,35 +1,179 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { ReusableFormComponent } from '../../ui/reusable-form/reusable-form.component';
 
+type CallBackFn = (formData: Record<string, number>) => void;
+type RentTab = 'gross' | 'net' | 'monthly';
 @Component({
   selector: 'app-rent',
   standalone: true,
-  imports: [
-    ReusableFormComponent,
-    ReactiveFormsModule
-  ],
+  imports: [ReusableFormComponent, ReactiveFormsModule],
   templateUrl: './rent.component.html',
-  styleUrl: './rent.component.scss'
+  styleUrl: './rent.component.scss',
 })
 export class RentComponent {
-  parentForm!: FormGroup;
-  formFields = [
-    { name: 'name', label: 'Name', type: 'text', validators: [Validators.required] },
-    { name: 'email', label: 'Email', type: 'email', validators: [Validators.required, Validators.email] }
+  showRentTab: RentTab = 'gross';
+  grossProfitabilityForm!: FormGroup;
+  grossProfitabilityFormFields = [
+    {
+      name: 'rent',
+      label: 'Loyer Mensuel',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'price',
+      label: 'Prix achat',
+      type: 'number',
+      validators: [Validators.required],
+    },
   ];
+  grossProfitabilityResult!: number;
+
+  netProfitabilityForm!: FormGroup;
+  netProfitabilityFormFields = [
+    {
+      name: 'rent',
+      label: 'Loyer Mensuel',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'price',
+      label: 'Prix achat',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'propertyTax',
+      label: 'Taxe foncière mensuelle',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'coOwnershipCharges',
+      label: 'Charges copropriété mensuelles',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'agencyFees',
+      label: 'Honoraires agence mensuelles',
+      type: 'number',
+      validators: [],
+    },
+  ];
+  netProfitabilityResult!: number;
+
+  monthlyProfitForm!: FormGroup;
+  monthlyProfitFormFields = [
+    {
+      name: 'rent',
+      label: 'Loyer mensuel',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'rentAssurance',
+      label: 'Assurance habitation mensuelle',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'credit',
+      label: 'Remboursement crédit mensuelle',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'creditAssurance',
+      label: 'Assurance crédit mensuelle',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'propertyTax',
+      label: 'Taxe foncière mensuelle',
+      type: 'number',
+      validators: [Validators.required],
+    },
+    {
+      name: 'agencyFees',
+      label: 'Honoraires agence mensuelles',
+      type: 'number',
+      validators: [],
+    },
+  ];
+  monthlyProfitResult!: number;
 
   constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit() {
-    this.parentForm = this.fb.group({
-      reusableForm: [null, Validators.required]
+    this.grossProfitabilityForm = this.fb.group({
+      reusableForm: [null, Validators.required],
+    });
+    this.netProfitabilityForm = this.fb.group({
+      reusableForm: [null, Validators.required],
+    });
+    this.monthlyProfitForm = this.fb.group({
+      reusableForm: [null, Validators.required],
     });
   }
 
-  onFormSubmit(formData: Record<string, unknown>) {
-    console.log('Form submitted:', formData);
-    // Handle form submission
+  callBackComputeGrossRent = (formData: Record<string, number>) => {
+    const { rent, price } = formData;
+    const annualRent = rent * 12;
+    this.grossProfitabilityResult = (annualRent / price) * 100;
+  };
+
+  callBackComputeNetRent = (formData: Record<string, number>) => {
+    const { rent, price, propertyTax, coOwnershipCharges, agencyFees } =
+      formData;
+    const annualRent =
+      rent * 12 -
+      -(propertyTax * 12) -
+      agencyFees * 12 -
+      coOwnershipCharges * 12;
+    this.netProfitabilityResult = (annualRent / price) * 100;
+  };
+
+  handleShowTab(tab: string) {
+    return this.showRentTab !== tab;
   }
 
+  showTab(tab: RentTab) {
+    this.showRentTab = tab;
+  }
+
+
+
+  callBackComputeMonthlyProfit = (formData: Record<string, number>) => {
+    const {
+      rent,
+      rentAssurance,
+      credit,
+      creditAssurance,
+      propertyTax,
+      agencyFees,
+    } = formData;
+    const charges =
+      Number(propertyTax) +
+      Number(agencyFees) +
+      Number(rentAssurance) +
+      Number(creditAssurance) +
+      Number(credit);
+    this.monthlyProfitResult = rent - charges;
+  };
+
+  onFormSubmit = (
+    formData: Record<string, number>,
+    callBack: CallBackFn
+  ): void => {
+    callBack(formData);
+  };
 }
